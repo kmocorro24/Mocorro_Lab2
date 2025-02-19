b76a6761eca97da39674a08250aa38301f6ad724
kmocorro24 <kmocorro24@gmail.com>
2025-02-19 10:21:33 +0800

    Update Welcome.jsx

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,18 @@
+
+function Welcome() {
+    return (
+      <div>
+         <div className="whopper">
+          <img src="../src/assets/pictures/whopper.jpg" alt="Whopper" />
+        </div>
+  
+        <div className="welcometext">
+          <h2>Welcome to Burger King. Home of the Whopper®!</h2>
+          <p>Hungry yet? Give in.</p>
+        </div>
+      </div>
+  
+    )
+  }
+
+  export default Welcome
